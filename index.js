fc6703cc884ade45762afe342469da167bf96f1d
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2021-03-23 14:39:34 +0200

    update package

---
+++ index.js
@@ -0,0 +1,55 @@
+"use strict";
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        Object.defineProperty(o, k2, {
+          enumerable: true,
+          get: function () {
+            return m[k];
+          },
+        });
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __setModuleDefault =
+  (this && this.__setModuleDefault) ||
+  (Object.create
+    ? function (o, v) {
+        Object.defineProperty(o, "default", { enumerable: true, value: v });
+      }
+    : function (o, v) {
+        o["default"] = v;
+      });
+var __importStar =
+  (this && this.__importStar) ||
+  function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null)
+      for (var k in mod)
+        if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k))
+          __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+  };
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = require("react");
+const Cookies = __importStar(require("js-cookie"));
+const useAbTest = (testName) => {
+  const [variant, setVariant] = react_1.useState(
+    Cookies.getJSON(testName) || null
+  );
+  react_1.useEffect(() => {
+    window.addEventListener(testName, (e) => {
+      setVariant(e.detail);
+      Cookies.set(testName, e.detail);
+    });
+  }, [testName]);
+  const isExperienceActive = variant === "variant1";
+  return { variant, isExperienceActive };
+};
+exports.default = useAbTest;
--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare type Props = {
-    variant?: 'variant1' | 'variant2';
-};
-declare const useAbTest: (testName: string) => Props;
-export default useAbTest;
